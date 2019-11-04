f56a3e8e22f1615ad6e396246cb5c5bd9dc7b03b
Serene <unexpecte-d@hotmail.com>
2019-11-05 01:35:57 +0800

    Simple refactor for clarity and add basic test case

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,13 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require('../src/gilded_rose');
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+describe('Gilded Rose', function() {
+	describe('- Miscellaneous Items', function() {
+		it('should return "foo", quality: 0, sellIn: -1', function() {
+			const gildedRose = new Shop([ new Item('foo', 0, 0) ]);
+			const items = gildedRose.updateQuality();
+			expect(items[0].name).toBe('foo');
+			expect(items[0].quality).toBe(0);
+			expect(items[0].sellIn).toBe(-1);
+		});
+	});
 });
