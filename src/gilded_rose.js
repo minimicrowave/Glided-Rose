b70891523e5bb0fd5e8cc7e445b82621e8b073a1
Serene <unexpecte-d@hotmail.com>
2019-11-05 16:47:10 +0800

    Add Legendary and BackStagePass class

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,6 +6,37 @@ class Item {
 	}
 }
 
+class BackStagePass extends Item {
+	updateQuality() {
+		if (this.sellIn < 0) {
+			this.quality = 0;
+		} else if (this.sellIn <= 5) {
+			this.quality += 3;
+		} else if (this.sellIn <= 10) {
+			this.quality += 2;
+		}
+
+		this.reduceSellIn();
+	}
+
+	reduceSellIn() {
+		this.sellIn -= 1;
+	}
+}
+
+class Legendary extends Item {
+	constructor(name, sellIn, quality) {
+		super(name, null, quality);
+	}
+	updateQuality() {
+		console.log('Legendary does not decrease in quality.');
+	}
+
+	reduceSellIn() {
+		console.log('Lengendary Item never has to be sold.');
+	}
+}
+
 class Shop {
 	constructor(items = []) {
 		this.items = items;
@@ -13,46 +44,50 @@ class Shop {
 
 	updateQuality() {
 		this.items = this.items.map((item) => {
-			if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-				if (item.quality > 0) {
-					if (item.name != 'Sulfuras, Hand of Ragnaros') {
-						item.quality -= 1;
-					}
-				}
+			if (item.quality) {
+				item.updateQuality();
 			} else {
-				if (item.quality < 50) {
-					item.quality += 1;
-					if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality += 1;
-							}
+				if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+					if (item.quality > 0) {
+						if (item.name != 'Sulfuras, Hand of Ragnaros') {
+							item.quality -= 1;
 						}
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality += 1;
-							}
-						}
-					}
-				}
-			}
-			if (item.name != 'Sulfuras, Hand of Ragnaros') {
-				item.sellIn = item.sellIn - 1;
-			}
-			if (item.sellIn < 0) {
-				if (item.name != 'Aged Brie') {
-					if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-						if (item.quality > 0) {
-							if (item.name != 'Sulfuras, Hand of Ragnaros') {
-								item.quality -= 1;
-							}
-						}
-					} else {
-						item.quality = item.quality - item.quality;
 					}
 				} else {
 					if (item.quality < 50) {
 						item.quality += 1;
+						if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+							if (item.sellIn < 11) {
+								if (item.quality < 50) {
+									item.quality += 1;
+								}
+							}
+							if (item.sellIn < 6) {
+								if (item.quality < 50) {
+									item.quality += 1;
+								}
+							}
+						}
+					}
+				}
+				if (item.name != 'Sulfuras, Hand of Ragnaros') {
+					item.sellIn = item.sellIn - 1;
+				}
+				if (item.sellIn < 0) {
+					if (item.name != 'Aged Brie') {
+						if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+							if (item.quality > 0) {
+								if (item.name != 'Sulfuras, Hand of Ragnaros') {
+									item.quality -= 1;
+								}
+							}
+						} else {
+							item.quality = item.quality - item.quality;
+						}
+					} else {
+						if (item.quality < 50) {
+							item.quality += 1;
+						}
 					}
 				}
 			}
@@ -63,7 +98,17 @@ class Shop {
 	}
 }
 
+function isWithinQualityRange(quality) {
+	return quality > 0 && quality < 50;
+}
+
+function isAfterConcert(day) {
+	day < 0;
+}
+
 module.exports = {
 	Item,
-	Shop
+	Shop,
+	Legendary,
+	BackStagePass
 };
--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,13 +1,33 @@
-const { Shop, Item } = require('../src/gilded_rose');
+const { Shop, Item, Legendary, BackStagePass} = require('../src/gilded_rose');
 
 describe('Gilded Rose', function() {
-	describe('- Miscellaneous Items', function() {
-		it('should return "foo", quality: 0, sellIn: -1', function() {
-			const gildedRose = new Shop([ new Item('foo', 0, 0) ]);
+	// describe('- Miscellaneous Items', function() {
+	// 	it('should return "foo", quality: 0, sellIn: -1', function() {
+	// 		const gildedRose = new Shop([ new Item('foo', 0, 0) ]);
+	// 		const items = gildedRose.updateQuality();
+	// 		expect(items[0].name).toBe('foo');
+	// 		expect(items[0].quality).toBe(0);
+	// 		expect(items[0].sellIn).toBe(-1);
+	// 	});
+	// });
+
+	describe('-BackStagePass', function(){
+		it('should return "Aged Brie", quality: 4, sellIn: 2', function() {
+			const gildedRose = new Shop([ new BackStagePass('Aged Brie', 3, 1) ]);
 			const items = gildedRose.updateQuality();
-			expect(items[0].name).toBe('foo');
-			expect(items[0].quality).toBe(0);
-			expect(items[0].sellIn).toBe(-1);
+			expect(items[0].name).toBe('Aged Brie');
+			expect(items[0].quality).toBe(4);
+			expect(items[0].sellIn).toBe(2);
 		});
-	});
+	})
+
+	describe('-BackStagePass', function(){
+		it('should return "Sulfuras", quality: 20, sellIn: null', function() {
+			const gildedRose = new Shop([ new Legendary('Sulfuras', 3, 20) ]);
+			const items = gildedRose.updateQuality();
+			expect(items[0].name).toBe('Sulfuras');
+			expect(items[0].quality).toBe(20);
+			expect(items[0].sellIn).toBe(null);
+		});
+	})
 });
